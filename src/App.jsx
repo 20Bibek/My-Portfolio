1d2023165d7dafbdd92e3118c6eb1926dd3ec471
Bibek Dash <bibektunu41@gmail.com>
2025-04-16 15:57:04 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import './App.css';
-import TopBox from './components/Topbox';
 import Experience from './components/Experience';
 import { useState } from 'react';
+import TopBox from './components/Topbox';
 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
   return (
       <Router>
         <div className="App">
-        <TopBox />
+        <TopBox/>
         <Routes>
           <Route path="/" element={<><About /><Skills /></>} />
           <Route path='/Experience' element={<Experience/>}/>
